bf5d071bbc060c68d4cfe330b6257d9c8217a0ec
dhanesh <dhaneshtekwani@gmail.com>
2022-10-29 23:51:54 +0530

    Updates

---
+++ precache-manifest.4bd19cbdb55b5e06e06bdbd6cf9e0206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc45e0c3fa53adbfe2ddd363347b2be0",
+    "url": "/codeeditor/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codeeditor/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "c151a87aa66a99d60195",
+    "url": "/codeeditor/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codeeditor/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codeeditor/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c151a87aa66a99d60195",
+    "url": "/codeeditor/static/js/main.db1df014.chunk.js"
+  },
+  {
+    "revision": "488aa566ef8aa777ad53",
+    "url": "/codeeditor/static/js/runtime-main.e1ae3f11.js"
+  }
+]);
